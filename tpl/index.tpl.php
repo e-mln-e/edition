041a5fe8cf3cd0aafa2b6c33831e0d10dc1414f7
Damien Senger <damien@bretzel-et-bredele.fr>
2014-05-26 19:04:18 +0200

    Mise en place Module TPL 2

--- tpl/index.tpl.php
+++ tpl/index.tpl.php
@@ -1,3 +1,107 @@
-<?php $core->tpl_load('header'); ?>
-Hello world!
-<?php $core->tpl_load('footer'); ?>
+<!doctype html>
+<!--[if lte IE 7]> <html class="no-js ie67 ie678" lang="fr"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8 ie678" lang="fr"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="fr"> <![endif]-->
+<!--[if gt IE 9]> <!--><html class="no-js" lang="fr"> <!--<![endif]-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+        <title>Edition numerique</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link rel="stylesheet" href="<?php tpl_go_to_assets('css'); ?>/style.css" media="all">
+        <link rel="stylesheet" href="<?php tpl_go_to_assets('css'); ?>/default.css" media="all">
+        <link rel="stylesheet" href="<?php tpl_go_to_assets('css'); ?>/component.css" media="all">
+    </head>
+    
+    <body class="cbp-spmenu-push">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="menu-general">
+			<a href="#">Accueil</a>
+			<a href="#">Nouvelle édition</a>
+			<a href="#">Collections</a>
+			<a href="#">Collaborateurs</a>
+			<a href="#">Mon profil</a>
+		</nav>
+        <header>
+            <div id="showLeftPush" class="w300p"><img src="<?php tpl_go_to_assets('img'); ?>/logo.jpg"/> EcrirePublier</div>
+        </header>
+        <main>
+            <h6 id="showLeft" class="w300p">Sommaire</h6>
+            <aside class="w300p sommaire cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="menu-sommaire">
+                <ul>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                </ul>
+            </aside>
+            <section class="text-edit">
+                <p>Lorem Salu bissame ! Wie geht's les samis ? Hans apporte moi une Wurschtsalad avec un picon bitte, s'il te plaît.
+  Voss ? Une Carola et du Melfor ? Yo dû, espèce de Knäckes, ch'ai dit un picon !</p> 
+                <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti de Bischheim était au Christkindelsmärik en compagnie de Richard Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère qu'ils avaient du Kabinetpapier, Gal !</p>
+                <p>Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.</p>
+                <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
+            </section>
+            <h6 id="showRight" class="w400p">Attributs</h6>
+            <aside class="w400p attributs cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="menu-attributs">
+                <ul>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                </ul>
+            </aside>
+        </main>
+        
+        <script src="<?php tpl_go_to_assets('js'); ?>/jquery.js"></script>
+        <script src="<?php tpl_go_to_assets('js'); ?>/classie.js"></script>
+        <script>
+			var menuGeneral = document.getElementById( 'menu-general' ),
+                menuSommaire = document.getElementById( 'menu-sommaire' ),
+				menuAttributs = document.getElementById( 'menu-attributs' ),
+				showLeft = document.getElementById( 'showLeft' ),
+				showRight = document.getElementById( 'showRight' ),
+				showLeftPush = document.getElementById( 'showLeftPush' ),
+				body = document.body;
+
+			showLeft.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( menuSommaire, 'cbp-spmenu-open' );
+				disableOther( 'showLeft' );
+			};
+			showRight.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( menuAttributs, 'cbp-spmenu-open' );
+				disableOther( 'showRight' );
+			};
+			showLeftPush.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( body, 'cbp-spmenu-push-toright' );
+				classie.toggle( menuGeneral, 'cbp-spmenu-open' );
+				disableOther( 'showLeftPush' );
+			};
+
+			function disableOther( button ) {
+				if( button !== 'showLeft' ) {
+					classie.toggle( showLeft, 'disabled' );
+				}
+				if( button !== 'showRight' ) {
+					classie.toggle( showRight, 'disabled' );
+				}
+				if( button !== 'showTop' ) {
+					classie.toggle( showTop, 'disabled' );
+				}
+				if( button !== 'showBottom' ) {
+					classie.toggle( showBottom, 'disabled' );
+				}
+				if( button !== 'showLeftPush' ) {
+					classie.toggle( showLeftPush, 'disabled' );
+				}
+				if( button !== 'showRightPush' ) {
+					classie.toggle( showRightPush, 'disabled' );
+				}
+			}
+		</script>
+    </body>
+</html>
