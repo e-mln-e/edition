aeb90a1aafaf9a5d78b03f26e3fb18f9f66ed98c
Damien Senger <damien@bretzel-et-bredele.fr>
2014-05-27 00:00:55 +0200

    Division fichier index en header et footer

--- tpl/footer.tpl.php
+++ tpl/footer.tpl.php
@@ -1,3 +1,54 @@
-	
-	</body>
+
+        
+        <script src="<?php $core->tpl_go_to_assets('js'); ?>/jquery.js"></script>
+        <script src="<?php $core->tpl_go_to_assets('js'); ?>/waypoints.js"></script>
+        <script src="<?php $core->tpl_go_to_assets('js'); ?>/classie.js"></script>
+        <script>
+			var menuGeneral = document.getElementById( 'menu-general' ),
+                menuSommaire = document.getElementById( 'menu-sommaire' ),
+				menuAttributs = document.getElementById( 'menu-attributs' ),
+				showLeft = document.getElementById( 'showLeft' ),
+				showRight = document.getElementById( 'showRight' ),
+				showLeftPush = document.getElementById( 'showLeftPush' ),
+				body = document.body;
+
+			showLeft.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( menuSommaire, 'cbp-spmenu-open' );
+				disableOther( 'showLeft' );
+			};
+			showRight.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( menuAttributs, 'cbp-spmenu-open' );
+				disableOther( 'showRight' );
+			};
+			showLeftPush.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( body, 'cbp-spmenu-push-toright' );
+				classie.toggle( menuGeneral, 'cbp-spmenu-open' );
+				disableOther( 'showLeftPush' );
+			};
+
+			function disableOther( button ) {
+				if( button !== 'showLeft' ) {
+					classie.toggle( showLeft, 'disabled' );
+				}
+				if( button !== 'showRight' ) {
+					classie.toggle( showRight, 'disabled' );
+				}
+				if( button !== 'showTop' ) {
+					classie.toggle( showTop, 'disabled' );
+				}
+				if( button !== 'showBottom' ) {
+					classie.toggle( showBottom, 'disabled' );
+				}
+				if( button !== 'showLeftPush' ) {
+					classie.toggle( showLeftPush, 'disabled' );
+				}
+				if( button !== 'showRightPush' ) {
+					classie.toggle( showRightPush, 'disabled' );
+				}
+			}
+		</script>
+    </body>
 </html>
--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -1,7 +1,30 @@
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>Édition numérique</title>
-	</head>
-	<body>
+<!doctype html>
+<!--[if lte IE 7]> <html class="no-js ie67 ie678" lang="fr"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8 ie678" lang="fr"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="fr"> <![endif]-->
+<!--[if gt IE 9]> <!--><html class="no-js" lang="fr"> <!--<![endif]-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+        <title>Edition numerique</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link rel="stylesheet" href="<?php $core->tpl_go_to_assets('css'); ?>/style.css" media="all">
+        <link rel="stylesheet" href="<?php $core->tpl_go_to_assets('css'); ?>/default.css" media="all">
+        <link rel="stylesheet" href="<?php $core->tpl_go_to_assets('css'); ?>/component.css" media="all">
+    </head>
+    
+    <body class="cbp-spmenu-push">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="menu-general">
+			<a href="#">Accueil</a>
+			<a href="#">Nouvelle édition</a>
+			<a href="#">Collections</a>
+			<a href="#">Collaborateurs</a>
+			<a href="#">Mon profil</a>
+		</nav>
+        <header>
+            <div id="showLeftPush" class="w300p"><img src="<?php $core->tpl_go_to_assets('img'); ?>/logo.jpg"/> EcrirePublier</div>
+        </header>
