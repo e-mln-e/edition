15913fb6bf15a70c43cb0ff9e2c95d59bf67719f
Damien Senger <damien@bretzel-et-bredele.fr>
2014-05-27 00:02:08 +0200

    Index

---
+++ tpl/index.tpl.php
@@ -0,0 +1,27 @@
+<?php tpl_load('header'); ?>
+        <main>
+            <h6 id="showLeft" class="w300p">Sommaire</h6>
+            <aside class="w300p sommaire cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="menu-sommaire">
+                <ul>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                </ul>
+            </aside>
+            <section class="text-edit">
+                <p>Lorem Salu bissame ! Wie geht's les samis ? Hans apporte moi une Wurschtsalad avec un picon bitte, s'il te plaît.
+  Voss ? Une Carola et du Melfor ? Yo dû, espèce de Knäckes, ch'ai dit un picon !</p> 
+                <p>Hopla vous savez que la mamsell Huguette, la miss Miss Dahlias du messti de Bischheim était au Christkindelsmärik en compagnie de Richard Schirmeck (celui qui a un blottkopf), le mari de Chulia Roberstau, qui lui trempait sa Nüdle dans sa Schneck ! Yo dû, Pfourtz ! Ch'espère qu'ils avaient du Kabinetpapier, Gal !</p>
+                <p>Yoo ch'ai lu dans les DNA que le Racing a encore perdu contre Oberschaeffolsheim. Verdammi et moi ch'avais donc parié deux knacks et une flammekueche. Ah so ? T'inquiète, ch'ai ramené du schpeck, du chambon, un kuglopf et du schnaps dans mon rucksack. Allez, s'guelt ! Wotch a kofee avec ton bibalaekaess et ta wurscht ? Yeuh non che suis au réchime, je ne mange plus que des Grumbeere light et che fais de la chym avec Chulien. Tiens, un rottznoz sur le comptoir.</p>
+                <p>Tu restes pour le lotto-owe ce soir, y'a baeckeoffe ? Yeuh non, merci vielmols mais che dois partir à la Coopé de Truchtersheim acheter des mänele et des rossbolla pour les gamins. Hopla tchao bissame ! Consectetur adipiscing elit</p>
+            </section>
+            <h6 id="showRight" class="w400p"><img src="<?php $core->tpl_go_to_assets('img'); ?>/sommaire-actif.jpg"/> Attributs</h6>
+            <aside class="w400p attributs cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="menu-attributs">
+                <ul>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                    <li>item 1</li>
+                </ul>
+            </aside>
+        </main>
+<?php tpl_load('footer'); ?>
