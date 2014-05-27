faf12f91a6f05bf84ab313140be95cdc727fe5ad
e-mln-e <brule.emeline@gmail.com>
2014-05-27 14:18:42 +0200

    html du login

--- tpl/login.tpl.php
+++ tpl/login.tpl.php
@@ -2,8 +2,11 @@
 <h4>Outil d'écriture et de publication numérique</h4>
 <p>Cet outil est en version bêta</p>
 <form>Veuillez vous identifier
-<br>
-<br>
-<button></button>
+<br><label for="login"><strong>Adresse mail</strong></label>
+    <input type="text" name="login" id="login"/>
+<br>    
+    <label for="pass"><strong>Mot de passe</strong></label>
+    <input type="password" name="pass" id="pass"/>
+<button type="submit" name="connexion">Se connecter</button>
 </form>
-<p><a href="mailto:emeline.brule@ensad.fr">Demander un</a></p>
+<p><a href="mailto:emeline.brule@ensad.fr">Demander un accès</a></p>
