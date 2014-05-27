13341e48ae4db75eb029e6e1dcbdafa1887837e4
e-mln-e <brule.emeline@gmail.com>
2014-05-27 11:30:51 +0200

    modifs index.tpl.php

--- tpl/index.tpl.php
+++ tpl/index.tpl.php
@@ -20,9 +20,17 @@
             <p id="showRight"></p>
                 <ul class="attributs cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="menu-attributs">
                     <h6>Attributs</h6>
-                    <li>Medias</li>
-                    <li>Mots-clés</li>
+                    <li id="mots-cles">Mots-clés</li>
                     <li>Collaborateurs</li>
+                    <li id="medias">Medias <button>Upload</button>
+                    </li>
+                    <li class="media-uploaded"><img src="http://www.picturetopeople.org/images/artistic_mosaic_photo_effect_input.jpg"/>
+                    <div class="metadata-medias"><br>Type : image 
+                    <br>Text-alternatif* : Ceci est un chat avec un chapeau. 
+                    <br>Mise en page :
+                    <br>
+                    </div>
+                    </li>
                 </ul>
             </aside>
         </main>
